88cb63bcfe42f0d268d64196e868a3e02da8726f
Yuri Soares <yurisoaresm@outlook.com>
2022-11-30 17:23:05 -0300

    Estrutura de páginas e rotas

---
+++ src/Pages/Start/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, Text, Image, ScrollView } from "react-native"; 
+
+export default function Start() {
+
+  return(
+    <View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View>
+          <Image
+            source={require("../../assets/icons/logo3.png")} 
+          />
+          <Text>
+            Vamos transformar sua vida {"/n"} em jogo, buscando sempre {"/n"} o melhor nível.
+          </Text>
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
---
+++ src/Routes/AllPages.jsx
@@ -0,0 +1,21 @@
+import { NavigationContainer, StackActions } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Start from "../Pages/Start/index";
+
+const Stack = createNativeStackNavigator();
+
+export default function AllPages() {
+  return(
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown:false,
+        }}
+      >
+
+        <Stack.Screen name="Start" component={Start} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
