97890e06a0db43f371619c5a6718559709587d63
Yuri Soares <yurisoaresm@outlook.com>
2022-12-02 18:06:08 -0300

    Botão de Criar Hábitos

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, ScrollView, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 import LifeStatus from "../../Components/Common/LifeStatus";
 import StatusBar from "../../Components/Home/StatusBar";
+import CreateHabit from "../../Components/Home/CreateHabit";
 
 export default function Home() {
   const navigation = useNavigation();
+  const [mindHabit, setMindHabit] = useState();
+  const [moneyHabit, setMoneyHabit] = useState();
+  const [bodyHabit, setBodyHabit] = useState();
+  const [funHabit, setFunHabit] = useState();
 
   function handleNavAppExplanation() {
     navigation.navigate("AppExplanation");
@@ -21,6 +26,7 @@ export default function Home() {
           </Text>
           <LifeStatus />
           <StatusBar />
+          <CreateHabit habitArea="Mente" borderColor={"#90B7F3"}/>
         </View>
         <Text 
           style={styles.explanationText}
